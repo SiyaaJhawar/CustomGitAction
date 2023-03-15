9bea854425b405315ae284b38ae16ce673d94dca
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-03-15 17:58:15 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,21 @@
-const fetch = require('node-fetch');
-fetch('https://example.com/api/data')
-  .then(response => response.json())
-  .then(data => {
-    // The data variable contains the parsed JSON response from the API.
-    // Iterate over the keys and values and log them to the console.
-    Object.keys(data).forEach(key => {
-      console.log(key + ': ' + data[key]);
-    });
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
 
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.publicapis.org/entries", true);
+xhr.send();
+
+xhr.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    var result = JSON.parse(this.responseText);
+
+    var entries = result.entries; // Get the entries array from the result object
+
+    entries.forEach(function (entry) {
+      var keys = Object.keys(entry); // Get the keys of the current entry object
+    
+      console.log("Key-Value Pairs:");
+      keys.forEach(function (key) {
+        console.log(`${key}: ${entry[key]}`);
+      });
+    });
+  }
