b1783e414d8b0d0ca2a0919d639d2864e2973a0d
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-03-16 11:11:32 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,49 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.publicapis.org/entries", true);
+xhr.open("GET", "https://www.boredapi.com/api/activity", true);
 xhr.send();
 
 xhr.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     var result = JSON.parse(this.responseText);
 
-    var entries = result.entries; // Get the entries array from the result object
+    var activityType = result.type; // Get the activity type from the result object
 
-    entries.forEach(function (entry) {
-      var keys = Object.keys(entry); // Get the keys of the current entry object
-    
-      console.log("Key-Value Pairs:");
-      keys.forEach(function (key) {
-        console.log(`${key}: ${entry[key]}`);
-      });
+    console.log(`Activity Type: ${activityType}`);
 
-    
-      if (entry.category === "HTTPS") {
-        console.log("Blocker found. Submitting PR for review...");
+    // Check if the activity is a blocker
+    if (activityType === "recreational") {
+      console.log("Blocker found. Submitting PR for review...");
 
-     
-        var prRequest = new XMLHttpRequest();
-        var url = "https://api.github.com/repos/SiyaaJhawar/CustomAction/pulls";
-        prRequest.open("POST", url, true);
-        prRequest.setRequestHeader("Content-Type", "application/json");
-        prRequest.setRequestHeader("Authorization", "Bearer {access_token}");
+      // Set up the request to create a new pull request
+      var prRequest = new XMLHttpRequest();
+      var url = "https://api.github.com/repos/{owner}/{repo}/pulls";
+      prRequest.open("POST", url, true);
+      prRequest.setRequestHeader("Content-Type", "application/json");
+      prRequest.setRequestHeader("Authorization", "Bearer {access_token}");
 
-        // Create the pull request payload
-        var payload = {
-          title: "Fix for blocker in API entry",
-          body: "This pull request fixes the blocker found in the API entry.",
-          head: "fix-blocker",
-          base: "master"
-        };
+      // Create the pull request payload
+      var payload = {
+        title: "Fix for blocker in Bored API",
+        body: "This pull request fixes the blocker found in the Bored API.",
+        head: "fix-blocker",
+        base: "master"
+      };
 
-        // Send the pull request payload
-        prRequest.send(JSON.stringify(payload));
+      // Send the pull request payload
+      prRequest.send(JSON.stringify(payload));
 
-        // Handle the response from the API
-        prRequest.onreadystatechange = function() {
-          if (this.readyState === 4 && this.status === 201) {
-            console.log("Pull request created successfully!");
-          } else if (this.readyState === 4 && this.status !== 201) {
-            console.log("Error creating pull request.");
-          }
+      // Handle the response from the API
+      prRequest.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 201) {
+          console.log("Pull request created successfully!");
+        } else if (this.readyState === 4 && this.status !== 201) {
+          console.log("Error creating pull request.");
         }
-      } else {
-        console.log("No blockers found. Continuing workflow...");
       }
-    });
+    } else {
+      console.log("No blockers found. Continuing workflow...");
+    }
   }
 };
 
