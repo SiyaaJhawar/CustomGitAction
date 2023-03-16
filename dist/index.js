ce8b7a35d3064ee79dd32721880e6be6f944878a
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-03-16 12:15:28 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -7,40 +7,45 @@ xhr.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     var result = JSON.parse(this.responseText);
 
-    var activityType = result.type; // Get the activity type from the result object
+    var keys = Object.keys(result); // Get the keys of the result object
+    
+    console.log("Key-Value Pairs:");
+    keys.forEach(function (key) {
+      console.log(`${key}: ${result[key]}`);
+    });
 
-    console.log(`Activity Type: ${activityType}`);
+    console.log("Blocker found. Submitting PR for review...");
 
-    // Check if the activity is a blocker
-    if (activityType != "recreational") {
-      console.log("Blocker found. Submitting PR for review...");
+    // Set up the request to create a new pull request
+    var prRequest = new XMLHttpRequest();
+    var url = "https://api.github.com/repos/SiyaaJhawar/CustomGitAction/pulls";
+    prRequest.open("POST", url, true);
+    prRequest.setRequestHeader("Content-Type", "application/json");
+    prRequest.setRequestHeader("Authorization", "Bearer {access_token}");
 
-      // Set up the request to create a new pull request
-      var prRequest = new XMLHttpRequest();
-var url = "https://api.github.com/repos/SiyaaJhawar/CustomGitAction/pulls";
-prRequest.open("POST", url, true);
-prRequest.setRequestHeader("Content-Type", "application/json");
-prRequest.setRequestHeader("Authorization", "Bearer {access_token}");
+    // Create the pull request payload
+    var payload = {
+      title: "Fix for blocker in Bored API",
+      body: "This pull request fixes the blocker found in the Bored API.",
+      head: "SiyaaJhawar-patch-1",
+      base: "main"
+    };
 
-// Create the pull request payload
-var payload = {
-  title: "Fix for blocker in Bored API",
-  body: "This pull request fixes the blocker found in the Bored API.",
-  head: "SiyaaJhawar-patch-1",
-  base: "main"
+    // Send the pull request payload
+    prRequest.send(JSON.stringify(payload));
+
+    // Handle the response from the API
+    prRequest.onreadystatechange = function() {
+      if (this.readyState === 4) {
+        if (this.status === 201) {
+          console.log("Pull request created successfully!");
+        } else {
+          console.log("Error creating pull request. Status code: " + this.status);
+          console.log("Response: " + this.responseText);
+        }
+      }
+    }
+  }
 };
 
-// Send the pull request payload
-prRequest.send(JSON.stringify(payload));
-
-// Handle the response from the API
-prRequest.onreadystatechange = function() {
-  console.log(`Ready state: ${this.readyState}, Status: ${this.status}`);
-  if (this.readyState === 4 && this.status === 201) {
-    console.log("Pull request created successfully!");
-  } else if (this.readyState === 4 && this.status !== 201) {
-    console.log("Error creating pull request.");
-    console.log(this.responseText);
-  }
-}
 
