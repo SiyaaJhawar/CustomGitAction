9ac9fadb49be850c45523f91c6b0df7d00bd4d2b
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-03-16 14:34:22 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -18,9 +18,9 @@ xhr.onreadystatechange = function () {
       // Set up the request to create a new pull request
       var prRequest = new XMLHttpRequest();
       var url = "https://api.github.com/repos/SiyaaJhawar/CustomGitAction/pulls";
-      prRequest.create("POST", url, true);
+      prRequest.open("POST", url, true);
       prRequest.setRequestHeader("Content-Type", "application/json");
-      prRequest.setRequestHeader("Authorization", "Bearer $secrets.GITHUB_TOKEN");
+     // prRequest.setRequestHeader("Authorization", "Bearer $secrets.GITHUB_TOKEN");
      // console.log("secrets.GITHUB_TOKEN");
 
       // Create the pull request payload
