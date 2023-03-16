2abdeedc37e769d3b4ac0b69a1575938080472f6
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-03-16 12:08:38 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -17,31 +17,32 @@ xhr.onreadystatechange = function () {
 
       // Set up the request to create a new pull request
       var prRequest = new XMLHttpRequest();
-      var url = "https://api.github.com/repos/SiyaaJhawar/CustomGitAction/pulls";
-      prRequest.open("POST", url, true);
-      prRequest.setRequestHeader("Content-Type", "application/json");
-      prRequest.setRequestHeader("Authorization", "Bearer {access_token}");
+var url = "https://api.github.com/repos/SiyaaJhawar/CustomGitAction/pulls";
+prRequest.open("POST", url, true);
+prRequest.setRequestHeader("Content-Type", "application/json");
+prRequest.setRequestHeader("Authorization", "Bearer {access_token}");
 
-      // Create the pull request payload
-      var payload = {
-        title: "Fix for blocker in Bored API",
-        body: "This pull request fixes the blocker found in the Bored API.",
-        head: "SiyaaJhawar-patch-1",
-        base: "main"
-      };
+// Create the pull request payload
+var payload = {
+  title: "Fix for blocker in Bored API",
+  body: "This pull request fixes the blocker found in the Bored API.",
+  head: "SiyaaJhawar-patch-1",
+  base: "main"
+};
 
-      // Send the pull request payload
-      prRequest.send(JSON.stringify(payload));
+// Send the pull request payload
+prRequest.send(JSON.stringify(payload));
 
-      // Handle the response from the API
-      prRequest.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 201) {
-          console.log("Pull request created successfully!");
-        } else if (this.readyState === 4 && this.status !== 201) {
-          console.log("Error creating pull request.");
-        }
-      }
-    } else {
+// Handle the response from the API
+prRequest.onreadystatechange = function() {
+  console.log(`Ready state: ${this.readyState}, Status: ${this.status}`);
+  if (this.readyState === 4 && this.status === 201) {
+    console.log("Pull request created successfully!");
+  } else if (this.readyState === 4 && this.status !== 201) {
+    console.log("Error creating pull request.");
+    console.log(this.responseText);
+  }
+}else {
       var keys = Object.keys(result); // Get the keys of the result object
       
       console.log("Key-Value Pairs:");
