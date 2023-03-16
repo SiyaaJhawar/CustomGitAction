1f8f6949cabffe7a6a4e1c7d0f8630cc5ea8fc35
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-03-16 10:34:11 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,3 @@
-
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.publicapis.org/entries", true);
@@ -17,6 +16,41 @@ xhr.onreadystatechange = function () {
       keys.forEach(function (key) {
         console.log(`${key}: ${entry[key]}`);
       });
+
+    
+      if (entry.blocker) {
+        console.log("Blocker found. Submitting PR for review...");
+
+     
+        var prRequest = new XMLHttpRequest();
+        var url = "https://api.github.com/repos/SiyaaJhawar/demo/pulls";
+        prRequest.open("POST", url, true);
+        prRequest.setRequestHeader("Content-Type", "application/json");
+        prRequest.setRequestHeader("Authorization", "Bearer {access_token}");
+
+        var payload = {
+          title: "Fix for blocker in API entry",
+          body: "This pull request fixes the blocker found in the API entry.",
+          head: "fix-blocker",
+          base: "main"
+        };
+
+      
+        prRequest.send(JSON.stringify(payload));
+
+        // Handle the response from the API
+        prRequest.onreadystatechange = function() {
+          if (this.readyState === 4 && this.status === 201) {
+            console.log("Pull request created successfully!");
+          } else if (this.readyState === 4 && this.status !== 201) {
+            console.log("Error creating pull request.");
+          }
+        }
+      } else {
+        console.log("No blocker found. Continuing workflow...");
+       
+      }
     });
   }
-}
+};
+
