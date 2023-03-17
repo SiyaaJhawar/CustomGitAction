0dbe9a977035ce450794c55244ffcaa5e2e48368
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-03-17 15:32:47 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -5,45 +5,44 @@ const octokit = new Octokit({
 });
 
 (async () => {
-  const response = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
-    owner: 'SiyaaJhawar',
-    repo: 'CustomGitAction'
-  });
-
-  const result = await octokit.request('GET /activity', {
-  baseUrl: 'https://www.boredapi.com/api',
-});
-
-const activityType = result.data.type;
-
-console.log(`Activity Type: ${activityType}`);
-
-if (activityType !== "recreational") {
-  console.log("Blocker found. Submitting PR for review...");
-
-  const payload = {
-    title: "Fix for blocker in Bored API",
-    body: "This pull request fixes the blocker found in the Bored API.",
-    head: "SiyaaJhawar-patch-1",
-    base: "main"
-  };
-
   try {
-    const prResponse = await octokit.request('POST /repos/{owner}/{repo}/pulls', {
+    const response = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
       owner: 'SiyaaJhawar',
-      repo: 'CustomGitAction',
-      ...payload
+      repo: 'CustomGitAction'
     });
 
-    console.log("Pull request created successfully!");
-  } catch (error) {
-    console.error("Error creating pull request: ", error.message);
-  }
-} else {
-  console.log("Key-Value Pairs:");
-  for (const [key, value] of Object.entries(result.data)) {
-    console.log(`${key}: ${value}`);
-  }
-}
+    const result = await octokit.request('GET /activity', {
+      baseUrl: 'https://www.boredapi.com/api',
+    });
 
-}
+    const activityType = result.data.type;
+
+    console.log(`Activity Type: ${activityType}`);
+
+    if (activityType !== "recreational") {
+      console.log("Blocker found. Submitting PR for review...");
+
+      const payload = {
+        title: "Fix for blocker in Bored API",
+        body: "This pull request fixes the blocker found in the Bored API.",
+        head: "SiyaaJhawar-patch-1",
+        base: "main"
+      };
+
+      const prResponse = await octokit.request('POST /repos/{owner}/{repo}/pulls', {
+        owner: 'SiyaaJhawar',
+        repo: 'CustomGitAction',
+        ...payload
+      });
+
+      console.log("Pull request created successfully!");
+    } else {
+      console.log("Key-Value Pairs:");
+      for (const [key, value] of Object.entries(result.data)) {
+        console.log(`${key}: ${value}`);
+      }
+    }
+  } catch (error) {
+    console.error("Error occurred: ", error.message);
+  }
+})();
