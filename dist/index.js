d1c7ba5d0e397d628fc79a0d8305b23fd9001352
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-03-17 16:52:07 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ const { Octokit } = require("octokit");
 // import { createPullRequest } from "octokit-plugin-create-pull-request";
 const { createPullRequest } = require("octokit-plugin-create-pull-request");
 //const Octokit = Octokit.plugin(createPullRequest);
-
+const TOKEN = "1234";
 const octokit = new Octokit({
   auth: TOKEN
 });
